a64f91e786ee242795cca35fc746cb6177acab3a
Roman <r.monahov2014@yandex.ru>
2020-01-20 14:50:39 +0300

    Initial Commit

---
+++ UISegmentControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISegmentControl
+//
+//  Created by Roman on 20.01.2020.
+//  Copyright © 2020 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
